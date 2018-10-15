09a729effa7d2c54532be1b899dc176403f36d50
Travis T <travistokuyama@gmail.com>
2018-10-15 19:58:39 +0000

    first commit

---
+++ lib/dc/michelin/guide.rb
@@ -0,0 +1,9 @@
+require "dc/michelin/guide/version"
+
+module Dc
+  module Michelin
+    module Guide
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/dc/michelin/guide/version.rb
@@ -0,0 +1,7 @@
+module Dc
+  module Michelin
+    module Guide
+      VERSION = "0.1.0"
+    end
+  end
+end
