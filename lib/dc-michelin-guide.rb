ceeb3fb0930edb7ea3edf0b5b4b90f968b105e01
Travis T <travistokuyama@gmail.com>
2018-10-17 14:41:25 +0000

    Updated required paths to reflect new file tree

--- lib/dc-michelin-guide.rb
+++ lib/dc-michelin-guide.rb
@@ -2,5 +2,6 @@ module DCMichelinGuide
 end
 
 require_relative '../config/environment'
-require_relative '../lib/cli.rb'
-require_relative '../lib/restaurant.rb'
+require_relative '../lib/dc-michelin-guide/cli.rb'
+require_relative '../lib/dc-michelin-guide/restaurant.rb'
+require_relative '../lib/dc-michelin-guide/scraper.rb'
